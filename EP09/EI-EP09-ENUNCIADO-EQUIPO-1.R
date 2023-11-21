cad463cc4765251907e02eb8de9654b19edf3a90
valenpy22 <valentina.campos.o@usach.cl>
2023-11-21 14:20:12 -0300

    fix: parte 8

--- EP09/EI-EP09-ENUNCIADO-EQUIPO-1.R
+++ EP09/EI-EP09-ENUNCIADO-EQUIPO-1.R
@@ -5,6 +5,7 @@ library(dplyr) # Para manipulación de datos
 library(multcomp) # Para comparaciones post-hoc
 library(ez)
 library(car)
+library(caret)
 
 # 1. Definir la semilla a utilizar, que corresponde a los últimos cuatro dígitos 
 # del RUN (sin considerar el dígito verificador) del integrante de menor edad del equipo.
@@ -12,7 +13,7 @@ set.seed(5689)
 
 # 2. Seleccionar una muestra de 50 mujeres (si la semilla es un número par) o 50 
 # hombres (si la semilla es impar).
-datos <- read.csv2("EP09 Datos.csv", fileEncoding = "UTF-8")
+datos <- read.csv2("Desktop/EI-EP-09-10-11/EP09/EP09 Datos.csv", fileEncoding = "UTF-8")
 
 head(datos)
 
@@ -111,18 +112,32 @@ print(ncvTest(modelo))
 # 8. Evaluar el poder predictivo del modelo en datos no utilizados para construirlo 
 # (o utilizando validación cruzada).
 
-n <- nrow(datos)
-n_entrenamiento <- floor(0.7 * n)
+full_model_formula <- Weight ~ Knees.diameter + Waist.Girth +
+  Hip.Girth + Ankle.Minimum.Girth + Thigh.Girth
 
-muestra <- sample.int(n = n, size = n_entrenamiento, replace = FALSE)
-entrenamiento <- datos[muestra, ]
-prueba <- datos[-muestra, ]
+# Preparar el dataframe con los predictores seleccionados
+predictors <- data.frame(Knees.diameter = muestra_hombres$Knees.diameter,
+                         Waist.Girth = muestra_hombres$Waist.Girth,
+                         Hip.Girth = muestra_hombres$Hip.Girth,
+                         Knee.Girth = muestra_hombres$Knee.Girth,
+                         Ankle.Minimum.Girth = muestra_hombres$Ankle.Minimum.Girth,
+                         Thigh.Girth = muestra_hombres$Thigh.Girth)
 
-mse_entrenamiento <- mean(modelo$residuals ** 2)
+# PPreparar la variable de salida
+outcome <- muestra_hombres$Weight
 
-predicciones <- predict(modelo, prueba)
+# Validación cruzada de 10 pliegues
+train_control <- trainControl(method = "cv", number = 10)
 
-error <- prueba[["Thigh.Girth"]] - predicciones
-mse_prueba <- mean(error ** 2)
+cv_model <- train(x = predictors, y = outcome,
+                  method = "lm",
+                  trControl = train_control)
 
-cat(mse_prueba)
+# Resultados de la validación cruzada
+print(cv_model)
+
+# Podemos decir que en base a los 3 resultados:
+# RMSE: 
+# R-squared: Se tiene un valor de 0,8427421, por lo que en un 84,27% de la variabilidad
+# den el peso puede ser explicada por las variables independientes del modelo.
+# MAE: 
