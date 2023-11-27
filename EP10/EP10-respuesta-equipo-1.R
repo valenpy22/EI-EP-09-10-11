59440bd0189df69e8b358880813df734c162bfe0
valenpy22 <valentina.campos.o@usach.cl>
2023-11-27 15:02:37 -0300

    lectura de archivos

--- EP10/EP10-respuesta-equipo-1.R
+++ EP10/EP10-respuesta-equipo-1.R
@@ -11,7 +11,7 @@ head(datos)
 
 datos[["IMC"]] <- datos[["Weight"]]/((datos[["Height"]])/100)^2
 
-datos[["EN"]] <- ifelse(datos[["IMC"]] >= 25, "Sobrepeso", "No sobrepeso")
+datos[["EN"]] <- ifelse(datos[["IMC"]] >= 25, 1, 0)
 
 head(datos)
 
@@ -21,14 +21,14 @@ set.seed(5547)
 
 # 2. Seleccionar una muestra de 90 mujeres (si la semilla es un número par) o 90 hombres (si la semilla es impar), asegurando que la mitad tenga estado nutricional “sobrepeso” y la otra mitad “no sobrepeso” en cada caso. Dividir esta muestra en dos conjuntos: los datos de 60 personas (30 con EN “sobrepeso”) para utilizar en la construcción de los modelos y 30 personas (15 con EN “sobrepeso”) para poder evaluarlos.
 hombres <- datos[datos$Gender == 1, ]
-sobrepeso <- hombres %>% filter(EN == "Sobrepeso") %>% sample_n(45)
-nosobrepeso <- hombres %>% filter(EN == "No sobrepeso") %>% sample_n(45)
+sobrepeso <- hombres %>% filter(EN == 1) %>% sample_n(45)
+nosobrepeso <- hombres %>% filter(EN == 0) %>% sample_n(45)
 
 muestra <- rbind(sobrepeso, nosobrepeso)
 
 set_construccion <- rbind(
-  sample_n(muestra %>% filter(EN == "Sobrepeso"), 30),
-  sample_n(muestra %>% filter(EN == "No sobrepeso"), 30)
+  sample_n(muestra %>% filter(EN == 1), 30),
+  sample_n(muestra %>% filter(EN == 0), 30)
 )
 
 set_evaluacion <- setdiff(muestra, set_construccion)
@@ -49,6 +49,27 @@ set_evaluacion <- setdiff(muestra, set_construccion)
 
 # 5. Usando el entorno R y paquetes estándares1, construir un modelo de regresión logística con el predictor seleccionado en el paso anterior y utilizando de la muestra obtenida.
 
+# Construir el modelo de regresión logística
+modelo_logistico <- glm(EN ~ Navel.Girth,
+                        family = binomial(link = "logit"), data = set_construccion)
+
+# Resumen del modelo
+summary(modelo_logistico)
+
+# Realizar predicciones sobre el conjunto de evaluación
+predicciones_prob <- predict(modelo_logistico, newdata = set_evaluacion, type = "response")
+predicciones <- ifelse(predicciones_prob > 0.5, 1, 0)
+
+# Crear una matriz de confusión para evaluar la sensibilidad y especificidad
+matriz_confusion <- table(Prediccion = predicciones, Real = set_evaluacion$EN)
+
+# Calculando sensibilidad y especificidad
+sensibilidad <- matriz_confusion[1, 1] / sum(matriz_confusion[, 1])
+especificidad <- matriz_confusion[0, 0] / sum(matriz_confusion[, 0])
+
+# Imprimir los resultados
+print(paste("Sensibilidad:", sensibilidad))
+print(paste("Especificidad:", especificidad))
 
 # 6. Usando herramientas estándares1 para la exploración de modelos del entorno R, buscar entre dos y cinco predictores de entre las variables seleccionadas al azar, recordadas en el punto 3, para agregar al modelo obtenido en el paso 5.
 # 7. Evaluar la confiabilidad de los modelos (i.e. que tengan un buen nivel de ajuste y son generalizables) y “arreglarlos” en caso de que tengan algún problema.
